cc7a063fc584e539b848bed2b137ebdb78c41583
Shaawtee <titilayokwevi@gmail.com>
2022-09-08 11:54:49 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,10 @@
 /**
  * main - Printing
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (Successful)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0)
+	return (0);
 }
