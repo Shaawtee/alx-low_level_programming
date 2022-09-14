8fb6d0ff81bcc40670252f613db30bf9f2d355f0
Shaawtee <titilayokwevi@gmail.com>
2022-09-14 10:33:30 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet -print the alphabet
+ *
+ * Return: Always success
+ */
+void print_alphabet(void)
+{
+
+int n;
+
+for (n = 'a'; n <= 'z'; n++)
+{
+_putchar(n);
+}
+_putchar('\n');
