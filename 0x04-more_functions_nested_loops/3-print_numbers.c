1ae8d5d869327876c99b9bbca9bb4d1dfef0b9de
Shaawtee <titilayokwevi@gmail.com>
2022-09-15 18:39:28 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -9,8 +9,5 @@ void print_numbers(void)
 {
 int i;
 
-        for (i = 0; i <= 9; i++)
-	       	_putchar(i + '0');
-
-	putchar('\n');
+        for (i = 0; i <= 9; i++) _putchar(i + '0') ;putchar('\n');
 }
