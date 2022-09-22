856465458f60a2fb2abc02cc4157e0353e298db7
Shaawtee <titilayokwevi@gmail.com>
2022-09-22 08:29:58 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,9 +1,8 @@
 #include "main.h"
-
 /**
  * _strcmp - Function which compare two strings and
  * @s1: first string
- * @s2: second string
+ * @s2:second string
  * Return:
  *                 returns zero if s1 == s2
  *                 returns negative number if s1 < s2
