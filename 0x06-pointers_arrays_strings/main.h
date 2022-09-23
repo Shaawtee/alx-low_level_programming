f91cfb7d94c0b272cc77c856d87655347668d654
Shaawtee <titilayokwevi@gmail.com>
2022-09-23 10:19:27 -0700

    main.h,102-magic.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,8 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
 
 void reverse_array(int *a, int n);
+void print_number(int n);
 void print_number(int n);
 void print_buffer(char *b, int size);
 int _strcmp(char *s1, char *s2);
@@ -15,6 +15,5 @@ char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
-char *add_strings(char *n1, char *n2, char *r, int r_index);
 
 #endif
