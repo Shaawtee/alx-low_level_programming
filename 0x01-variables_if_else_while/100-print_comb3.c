aabf48e38869dbc7f202f9cf6200c315fa408547
Shaawtee <titilayokwevi@gmail.com>
2022-09-24 09:39:24 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,41 +13,29 @@
  */
 int main(void)
 {
-
 	int i, j, k;
 
-
-
 	i = 0;
-
-
 
 	while (i < 100)
 
 	{
 
 		j = i % 10; /* singles digit */
-
 		k = i / 10; /* doubles digit */
-
-
 
 		if (k < j)
 
 		{
 
 			putchar(k + '0');
-
 			putchar(j + '0');
-
-
 
 			if (i < 89)
 
 			{
 
 				putchar(44);
-
 				putchar(32);
 
 			}
@@ -55,11 +43,9 @@ int main(void)
 		}
 
 
-
 		i++;
 
 	}
-
 	putchar('\n');
 
 
